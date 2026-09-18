2795e0a681a089a7551d99983e55d4c8421172b8
manuel-ale-sistem <mmendezarizmendi02@gmail.com>
2026-09-18 13:39:33 -0600

    Update supabase_config.py

--- supabase_config.py
+++ supabase_config.py
@@ -1,8 +1,8 @@
 from supabase import create_client
-import os
+import streamlit as st
 
-SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+SUPABASE_URL = st.secrets["SUPABASE_URL"]
+SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
 
 supabase = create_client(
     SUPABASE_URL,
