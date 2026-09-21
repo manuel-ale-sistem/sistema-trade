9b1c1d10957c6610d4fd736aef8ba7436ec4a8bc
manuel-ale-sistem <mmendezarizmendi02@gmail.com>
2026-09-21 13:57:54 -0600

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,13 +1,18 @@
-import streamlit as st
 import bcrypt
 from supabase_config import supabase
 from services.accesos_service import registrar_acceso
 
 
+def verificar_password(password, password_hash):
+    return bcrypt.checkpw(
+        password.encode("utf-8"),
+        password_hash.encode("utf-8")
+    )
+
+
 def login(usuario, password):
 
     try:
-
         response = (
             supabase.table("usuarios")
             .select("*")
@@ -16,45 +21,27 @@ def login(usuario, password):
             .execute()
         )
 
-        st.write("DEBUG RESPONSE:", response.data)
-
         datos = response.data
 
         if not datos:
-            st.error("DEBUG: Usuario no encontrado")
             return None
 
         usuario_db = datos[0]
 
-        st.write("DEBUG USUARIO:", usuario_db["usuario"])
-        st.write("DEBUG HASH:", usuario_db["password"])
-
-        resultado = bcrypt.checkpw(
-            password.encode("utf-8"),
-            usuario_db["password"].encode("utf-8")
+        resultado = verificar_password(
+            password,
+            usuario_db["password"]
         )
 
-        st.write("DEBUG PASSWORD OK:", resultado)
-
         if resultado:
-            registrar_acceso(usuario, "LOGIN")
-            return usuario_db
-
-    except Exception as e:
-        st.error(f"DEBUG ERROR: {e}")
-
-    return None
-
             registrar_acceso(
                 usuario,
                 "LOGIN"
             )
-
             return usuario_db
 
-        print("❌ Contraseña incorrecta")
+        return None
 
     except Exception as e:
-        print("ERROR LOGIN:", e)
-
-    return None
+        print(f"Error login: {e}")
+        return None
