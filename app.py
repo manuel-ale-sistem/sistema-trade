b877479a88bd0651d2b850ed712ec8e6dcb3a624
manuel-ale-sistem <mmendezarizmendi02@gmail.com>
2026-09-24 12:59:16 -0600

    Update app.py

--- app.py
+++ app.py
@@ -181,9 +181,9 @@ elif menu == "Accesos":
     from pages.accesos import accesos
     accesos()
 
-elif menu == "Trade AI":
-    from pages.trade_ai import trade_ai
-    trade_ai()
+elif menu == "Asistente IA":
+    from pages.ia_asistente import ia_asistente
+    ia_asistente()
 
 # ==========================================
 # PIE
